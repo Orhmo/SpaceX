37b9c743b57389caae3993194933695e3e4eadd9
Orhmo <oorhmo@gmail.com>
2023-02-20 07:20:18 +0100

    Feat: Update

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -15,13 +15,13 @@ const Auth: React.FC = () => {
   }, [])
 
   return (
-    <div className='flex h-screen font-mono tracking-tighter'>
+    <div className='flex h-screen tracking-tighter'>
       <div className='hidden md:block md:w-[45%] bg-cover bg-center'>
         <div data-aos='fade-right' data-aos-easing='ease-in-sine' data-aos-duration={1000}>
           <Welcome />
         </div>
       </div>
-      <div className='w-full md:w-[55%] bg-[#FEF0F2] px-16 py-8 md:p-16'>
+      <div className='w-full md:w-[55%] bg-[#FEF0F2] px-16 py-8 md:px-16'>
         <div data-aos='fade-left' data-aos-easing='ease-in-sine' data-aos-duration={2000}>
           <UserContextProvider>
             <Login />
--- src/utils/Data.tsx
+++ src/utils/Data.tsx
@@ -6,7 +6,7 @@ const Blob = [
   {
     src: One,
     style:
-      'justify-center bg-[#FEF0F2] border-[#FCB6C0] border-[1px] rounded-2xl pt-[8px] overflow-hidden w-[100px] h-[65px]',
+      'justify-center bg-[#FEF0F2] border-[#FCB6C0] border-[1px] rounded-2xl pt-[4px] overflow-hidden w-[100px] h-[80px]',
     head: 'Monitor your Earnings',
     des: 'Easily see how much your busineses are earning on each transaction and watch your earnings rise.',
   },
@@ -14,7 +14,7 @@ const Blob = [
   {
     src: Two,
     style:
-      'bg-[#E1F3FF] border-[#B3E2FE] border-[1px] rounded-2xl pt-[9px] overflow-hidden w-[100px] h-[65px]',
+      'bg-[#E1F3FF] border-[#B3E2FE] border-[1px] rounded-2xl pt-[4px] overflow-hidden w-[100px] h-[80px]',
     head: 'Manage your Businesses',
     des: 'Easily see how much your businesses are earning on each transaction and watch your earnings rise.',
   },
@@ -23,7 +23,7 @@ const Blob2 = [
   {
     src: Three,
     style:
-      'bg-[#FEF7E1] border-[#FDEDBE] border-[1px] rounded-2xl pt-[7px] overflow-hidden w-[100px] h-[65px] ',
+      'bg-[#FEF7E1] border-[#FDEDBE] border-[1px] rounded-2xl pt-[4px] overflow-hidden w-[100px] h-[80px] ',
     head: 'Delegate to Staff',
     des: 'Easily see how much your businesses are earning on each transaction and watch your earnings rise.',
   },
