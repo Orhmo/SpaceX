13409967c390f332b64ac2f0cad94678cf835d6a
Orhmo <oorhmo@gmail.com>
2023-02-20 04:45:15 +0100

    Feat: Updatea

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -9,7 +9,7 @@ const Dashboard: React.FC = () => {
     const timeout = setTimeout(() => {
       // Log user out by redirecting them to login page
       window.location.href = '/'
-    }, 60000) // 2 minutes
+    }, 12000) // 2 minutes
 
     return () => clearTimeout(timeout)
   }, [])
